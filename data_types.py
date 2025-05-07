72b1151e538e93e779d2996db2f1179da89658d4
sourav-learning <chatterjee_sourav2003@yahoo.co.in>
2025-05-07 23:01:11 +0530

    list vs tuple

--- data_types.py
+++ data_types.py
@@ -32,3 +32,32 @@ listOfNames.sort()
 print("listOfNames in alphabetical order ", listOfNames)  # ['Amit', 'Arnab', 'Sourav', 'Suman', 'Zoheb']
 listOfNames.sort(reverse=True)
 print("listOfNames in reverse alphabetical order ", listOfNames)  # ['Zoheb', 'Suman', 'Sourav', 'Arnab', 'Amit']
+listOfNames.insert(2, "Satyajit")
+print("listOfNames after inserting Satyajit at 2nd position ", listOfNames)  # ['Zoheb', 'Suman', 'Satyajit', 'Sourav', 'Arnab', 'Amit']    
+
+listOfNames.remove("Arnab")
+print("listOfNames after removing Arnab ", listOfNames)  # ['Zoheb', 'Suman', 'Satyajit', 'Sourav', 'Amit']
+
+listOfNames.append("Joy")
+listOfNames.sort()
+print("listOfNames in alphabetical order after appending Joy ", listOfNames)  # ['Amit', 'Joy', 'Satyajit', 'Sourav', 'Suman', 'Zoheb']
+
+print("Print twice :",listOfNames*2)  # ['Amit', 'Joy', 'Satyajit', 'Sourav', 'Suman', 'Zoheb', 'Amit', 'Joy', 'Satyajit', 'Sourav', 'Suman', 'Zoheb']
+
+# Play with tuple
+listOfRollNos = (1, 5, 7, 2, 8, 3)
+print("listOfRollNos is of type ", type(listOfRollNos))  # <class 'tuple'>
+print("3rd roll no in the list is ", listOfRollNos[2])
+print("sorted list of roll no is ", sorted(listOfRollNos))  # [1, 2, 3, 5, 7, 8]
+#listOfRollNos.sort(reverse=True)
+# AttributeError: 'tuple' object has no attribute 'sort' However sorted method does not modify the 
+# original tuple. It returns a new sorted list.
+
+listOfCountries = ["India", "USA", "UK", "China", "Japan"]
+print("listOfCountries: ", listOfCountries) # ['India', 'USA', 'UK', 'China', 'Japan']
+listOfCountries.append("Nepal")  
+print("listOfCountries after appending Nepal: ", listOfCountries) # ['India', 'USA', 'UK', 'China', 'Japan', 'Nepal']
+listOfCountries.pop(2)
+print("listOfCountries after removing 2nd element: ", listOfCountries) # ['India', 'USA', 'China', 'Japan', 'Nepal']
+listOfCountries.insert(2, "Spain")
+print("listOfCountries after inserting Spain at 2nd position: ", listOfCountries) # ['India', 'USA', 'Spain', 'China', 'Japan', 'Nepal']
